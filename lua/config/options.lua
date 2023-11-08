1172a3d4b0608db6a5d8ddfc57e09fd76bfe5357
Ch4ni <Ch4ni@adahome.dev>
2023-11-08 11:31:49 -0800

    update config with desired plugins/extensions ...
    Enable a bunch of UI/language/feature extensions/plugins used for
    various common tasks in my workflow.
    
    These have been under some local iteration and are ready for sync.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local home_path = vim.fn.expand("$HOME")
+
+-- Set up vim data paths ... outside of $PWD to avoid adding unnecessary
+-- files to source code managed paths, device paths, etc.
+vim.opt.dir = home_path .. "/.tmp/nvim/swap"
+vim.opt.undodir = home_path .. "/.tmp/nvim/undo"
+vim.opt.backupdir = home_path .. "/.tmp/nvim/backup"
+
+-- set the undo history size
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
+
+-- ensure all datafile types are enabled
+vim.opt.backup = true
+vim.opt.swapfile = true
+vim.opt.undofile = true
+
+-- Pretty line-wrapping
+vim.opt.breakindent = true
+vim.opt.breakindentopt = "shift:2,min:40,sbr"
+vim.opt.linebreak = true
+vim.opt.wrap = true
